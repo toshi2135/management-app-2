e7d646e92451917ba13e1338bc9f557df505dc0e
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-04-10 01:35:00 +0700

    Create project

---
+++ HelloWpf/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+const int start = 1;
+int end = 10;
+
+int sum = 0;
+for (int i = start; i <= end; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Sum of {start} to {end} is {sum}");
