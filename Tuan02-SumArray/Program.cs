77134129588d1874d4a99265bcbdaf530983cde1
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-04-16 01:13:17 +0700

    Up until 18 mins

--- Tuan02-SumArray/Program.cs
+++ Tuan02-SumArray/Program.cs
@@ -26,7 +26,7 @@ int a = -99;
 int result_2 = a > 0 ? a : -a;
 Console.WriteLine(result_2);
 
-// Kiểu dữ liệu ?
+// Kiểu dữ liệu ? nhận giá trị null
 int? a_3 = null;
 int result_3 = a_3 ?? 99; // a != null ? a : 99;
 Console.WriteLine(result_3);
