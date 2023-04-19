f1aa21c6e7ec77dcd2e1124c6681af1beba1e3b0
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-04-20 02:28:31 +0700

    Create Tuan03

---
+++ Tuan03-OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+class Employee
+{
+    // Attributes
+    public string FirstName;
+    public string LastName;
+    public string Tel;
+}
+
+Employee Alice = new Employee(); //
