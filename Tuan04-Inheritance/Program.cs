291965352184b9f6b0b9c8dcb2230b5f0cf6218a
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-04-30 15:37:29 +0700

    Tuan04 - Inheritance

---
+++ Tuan04-Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Point2D
+{
+    public int x, y;
+    public Point2D(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public override string ToString()
+    {
+        return $"({x}, {y})";
+    }
+}
