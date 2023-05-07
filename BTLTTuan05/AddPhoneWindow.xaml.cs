7718bb7b32a6ec076fe293328d4e61f45df35c7c
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-05-07 23:13:39 +0700

    BTLTTuan05

---
+++ BTLTTuan05/AddPhoneWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BTLTTuan05
+{
+    /// <summary>
+    /// Interaction logic for AddPhoneWindow.xaml
+    /// </summary>
+    public partial class AddPhoneWindow : Window
+    {
+        public string Phone { get; set; } = "";
+
+        public AddPhoneWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Phone = txtPhone.Text;
+            DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
