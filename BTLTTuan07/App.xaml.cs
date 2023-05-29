3fb8474d64b93dc7911a2538bf113e3e74fb6e9f
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-05-29 15:45:43 +0700

    BTLTTuan07

--- BTLTTuan07/App.xaml.cs
+++ BTLTTuan07/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace BTLTTuan07_DataBinding
+namespace BTLTTuan07
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- BTLTTuan07/MainWindow.xaml.cs
+++ BTLTTuan07/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BTLTTuan07_DataBinding
+namespace BTLTTuan07
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
