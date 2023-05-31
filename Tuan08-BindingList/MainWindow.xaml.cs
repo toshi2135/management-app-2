f7c955e5b3bf81bff01f88cc5cb7bafa51d0c050
Tuan Nguyen <anhtuan.nguyenduc@gmail.com>
2023-06-01 03:09:40 +0700

    Finish Tuan08

--- Tuan08-BindingList/MainWindow.xaml.cs
+++ Tuan08-BindingList/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,21 +40,67 @@ namespace Tuan08_BindingList
         ObservableCollection<Student> _students;
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            _students = new ObservableCollection<Student>()
-            {
-                new Student() {ID="001", Name="Nguyen Van A", Avatar="Images/01.jpg"},
-                new Student() {ID="002", Name="Tran Van B", Avatar="Images/44.jpg"},
-                new Student() {ID="003", Name="Pham Van C", Avatar="Images/46.jpg"},
-                new Student() {ID="004", Name="Truong Van D", Avatar="Images/47.jpg"},
-                new Student() {ID="005", Name="Dao Van E", Avatar="Images/63.jpg"},
-                new Student() {ID="006", Name="Vo Van F", Avatar="Images/86.jpg"},
-                new Student() {ID="007", Name="Phan Van G", Avatar="Images/95.jpg"},
-                new Student() {ID="008", Name="Le Van H", Avatar="Images/97.jpg"},
-                new Student() {ID="009", Name="Lam Van I", Avatar="Images/98.jpg"},
-                new Student() {ID="010", Name="Doan Van J", Avatar="Images/99.jpg"},
-            };
+            StudentDAO DAO = new StudentDAO();
+            _students = DAO.GetStudents();
+
+            //_students = new ObservableCollection<Student>()
+            //{
+            //    new Student() {ID="001", Name="Nguyen Van A", Avatar="Images/01.jpg"},
+            //    new Student() {ID="002", Name="Tran Van B", Avatar="Images/44.jpg"},
+            //    new Student() {ID="003", Name="Pham Van C", Avatar="Images/46.jpg"},
+            //    new Student() {ID="004", Name="Truong Van D", Avatar="Images/47.jpg"},
+            //    new Student() {ID="005", Name="Dao Van E", Avatar="Images/63.jpg"},
+            //    new Student() {ID="006", Name="Vo Van F", Avatar="Images/86.jpg"},
+            //    new Student() {ID="007", Name="Phan Van G", Avatar="Images/95.jpg"},
+            //    new Student() {ID="008", Name="Le Van H", Avatar="Images/97.jpg"},
+            //    new Student() {ID="009", Name="Lam Van I", Avatar="Images/98.jpg"},
+            //    new Student() {ID="010", Name="Doan Van J", Avatar="Images/99.jpg"},
+            //};
 
             studentsComboBox.ItemsSource = _students;
+        }
+
+        class StudentDAO
+        {
+            public ObservableCollection<Student> GetStudents()
+            {
+                var students = new ObservableCollection<Student>();
+                //    {
+                //        new Student() {ID="001", Name="Nguyen Van A", Avatar="Images/01.jpg"},
+                //        new Student() {ID="002", Name="Tran Van B", Avatar="Images/44.jpg"},
+                //        new Student() {ID="003", Name="Pham Van C", Avatar="Images/46.jpg"},
+                //        new Student() {ID="004", Name="Truong Van D", Avatar="Images/47.jpg"},
+                //        new Student() {ID="005", Name="Dao Van E", Avatar="Images/63.jpg"},
+                //        new Student() {ID="006", Name="Vo Van F", Avatar="Images/86.jpg"},
+                //        new Student() {ID="007", Name="Phan Van G", Avatar="Images/95.jpg"},
+                //        new Student() {ID="008", Name="Le Van H", Avatar="Images/97.jpg"},
+                //        new Student() {ID="009", Name="Lam Van I", Avatar="Images/98.jpg"},
+                //        new Student() {ID="010", Name="Doan Van J", Avatar="Images/99.jpg"},
+                //    };
+
+                string data = "data.txt";
+                StreamReader reader = new StreamReader(data);
+                string buffer = reader.ReadLine();
+                int count = int.Parse(buffer);
+                for (int i = 1; i <= count; i++)
+                {
+                    buffer = reader.ReadLine();
+                    var tokens = buffer.Split(new string[] { "," }, StringSplitOptions.None);
+                    string id = tokens[0];
+                    string name = tokens[1];
+                    string avatar = tokens[2];
+                    students.Add(new Student()
+                    {
+                        ID = id,
+                        Name = name,
+                        Avatar = avatar
+                    });
+                }
+
+                reader.Close();
+                
+                return students;
+            }
         }
 
         private void addStudent_Click(object sender, RoutedEventArgs e)
@@ -68,6 +115,26 @@ namespace Tuan08_BindingList
 
         private void deleteStudent_Click(object sender, RoutedEventArgs e)
         {
+            DeleteSelectedStudent();
+        }
+
+        private void updatetudent_Click(object sender, RoutedEventArgs e)
+        {
+            EditSelectedStudent();
+        }
+
+        private void deleteMenu_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteSelectedStudent();
+        }
+
+        private void editMenu_Click(object sender, RoutedEventArgs e)
+        {
+            EditSelectedStudent();
+        }
+
+        private void DeleteSelectedStudent()
+        {
             int i = studentsComboBox.SelectedIndex;
             if (i >= 0)
             {
@@ -75,7 +142,7 @@ namespace Tuan08_BindingList
             }
         }
 
-        private void updatetudent_Click(object sender, RoutedEventArgs e)
+        private void EditSelectedStudent()
         {
             int i = studentsComboBox.SelectedIndex;
             if (i >= 0)
@@ -84,6 +151,12 @@ namespace Tuan08_BindingList
                 _students[i].Name = "Nguyen Van L";
                 _students[i].Avatar = "Images/36.jpg";
             }
+        }
+
+        private void listViewItem_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            int i = studentsComboBox.SelectedIndex;
+            MessageBox.Show($"ID: {_students[i].ID}\nName: {_students[i].Name}");
         }
     }
 }
